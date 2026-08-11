67354acc7ea45e5a044ef0c000257f0718aab098
Mohamed Osman <abdikafimohamed542@gmail.com>
2026-08-11 19:40:08 +0300

    chore: initialize SpendWise project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl bg-green-300 text-yellow-300'>
+      Mohamed Osman Ali
+    </div>
+  )
+}
+
+export default App
